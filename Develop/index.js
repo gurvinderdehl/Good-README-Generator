ad068a0aad88bb77752866b5536ea96f57319f42
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-02-21 12:53:33 +0000

    Created a function to generate readme

--- Develop/index.js
+++ Develop/index.js
@@ -61,6 +61,35 @@ function promptUser() {
         ]);
       }
 
+
+      function generateREADME(answers) {
+        return `# ${answers.project_title}
+          
+      #### Table of Contents
+      1. [Project Description](#project-description)
+      2. [Installation Instructions](#installation-instructions)
+      3. [Usage Information](#usage-information)
+      4. [Contributors](#contributors)
+      5. [Test Instructions](#test-instructions)
+      6. [License](#license)
+      7. [Questions](#questions)
+      ## Project Description
+      * ${answers.description}
+      ## Installation Instructions
+      * ${answers.install}
+      ## Usage Information
+      * ${answers.use}
+      ## Contributors
+      * ${answers.contributions}
+      ## Test Instructions
+      * ${answers.test}
+      ## License
+      * licensed under the ${answers.license}
+      ## Questions
+      * For additional help or questions, please reach out to ${answers.email}
+      * Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
+        
+      }
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
