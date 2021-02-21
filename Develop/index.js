0fcf87a28fffc66879d1228e185b16edfbacb9bd
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-02-21 12:22:14 +0000

    Created variables to include required packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = [];
